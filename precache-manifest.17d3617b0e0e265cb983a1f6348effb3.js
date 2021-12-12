20ca7ff571ab774b526e33429469baef21e139ce
Emily Chin <emilychin1130@gmail.com>
2021-12-11 22:24:47 -0800

    Updates

---
+++ precache-manifest.17d3617b0e0e265cb983a1f6348effb3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c67b433e485ae1d4f16644a29400bd51",
+    "url": "/cs124/index.html"
+  },
+  {
+    "revision": "56b0d5ffd73a40304ae9",
+    "url": "/cs124/static/css/2.171a7357.chunk.css"
+  },
+  {
+    "revision": "e72ab5c08d612e8174dd",
+    "url": "/cs124/static/css/main.7ad94486.chunk.css"
+  },
+  {
+    "revision": "56b0d5ffd73a40304ae9",
+    "url": "/cs124/static/js/2.24ef4df4.chunk.js"
+  },
+  {
+    "revision": "aa5b720e531adf408ba3",
+    "url": "/cs124/static/js/3.96b1931c.chunk.js"
+  },
+  {
+    "revision": "e72ab5c08d612e8174dd",
+    "url": "/cs124/static/js/main.56dad12f.chunk.js"
+  },
+  {
+    "revision": "b2ddeb2eb1d2e804b4c3",
+    "url": "/cs124/static/js/runtime~main.b8b94037.js"
+  },
+  {
+    "revision": "7897e44a4d1798a948414feb991fc34c",
+    "url": "/cs124/static/media/logo.7897e44a.png"
+  }
+]);
